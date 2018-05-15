74a9db21d88300cf3ead0ff99d4614256b4bc4ea
radikushch <radikushch@gmail.com>
2018-05-15 19:56:12 +0300

    Initial commit

---
+++ CSharpLab1/CSharpLab1/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharplab1
+{
+    class Program
+    {
+        private static int Nmax = 10, N_St = Nmax * (Nmax - 1) / 2;
+
+        private static int[,] A;
+        private static int[,] A_Eiler;
+        private static int[] Stack = new int[0];
+        private static int[][] Ribs;
+
+        static void Main(string[] args)
+        {
+            Init();
+            FindTree(A_Eiler);
+            PrintMatrix();
+            FindWay(0);
+            OutPut();
+            Console.ReadKey();
+        }
+
+        private static void Init()
+        {
+            Console.WriteLine("Input number of abonents");
+            Nmax = Convert.ToInt32(Console.ReadLine());
+            N_St = Nmax * (Nmax - 1) / 2;
+            A = new int[Nmax, Nmax];
+            A_Eiler = new int[Nmax, Nmax];
+            Ribs = new int[Nmax - 1][];
+            Console.WriteLine("1 - Generate matrix\n2 - Input matrix");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 2) InputMatrix(Nmax, A);
+            else GenerateMatrix(Nmax, A);
+        }
+
+        private static void InputMatrix(int size, int[,] A)
+        {
+            for (int i = 0; i < size; i++)
+                for (int j = i + 1; j < size; j++)
+                {
+                    Console.WriteLine("Enter a payment between x{0} and x{1}", i + 1, j + 1);
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                    A[j, i] = A[i, j];
+                }
+        }
+
+        private static void GenerateMatrix(int size, int[,] A)
+        {
+            Random random = new Random();
+            for (var i = 0; i < size; i++)
+            {
+                for (var j = 0; j < i; j++) A[i, j] = A[j, i];
+                int maxCon = size - i - 1;
+                int totalCon = random.Next(0, maxCon);
+                int[] conIndexses = new int[totalCon];
+                for (int j = 0; j < totalCon; j++)
+                    while (true)
+                    {
+                        int index = random.Next(i + 1, size);
+                        if (Array.IndexOf(conIndexses, index) != -1) continue;
+                        conIndexses[j] = index;
+                        break;
+                    }
+                foreach (int index in conIndexses) A[i, index] = random.Next(1, 100);
+                if (i != size - 1 && A[i, i + 1] == 0) A[i, i + 1] = random.Next(1, 100);
+            }
+        }
+
+        private static void FindTree(int[,] A_Eiler)
+        {
+            ISet<int> Sp = new HashSet<int>();
+            int min = 100;
+            int l = 0, t = 0;
+            for (int i = 0; i < Nmax - 1; i++)
+                for (int j = 1; j < Nmax; j++)
+                    if ((A[i, j] < min) && (A[i, j] != 0))
+                    {
+                        min = A[i, j];
+                        l = i;
+                        t = j;
+                    }
+            A_Eiler[l, t] = A[l, t];
+            A_Eiler[t, l] = A[t, l];
+            Sp.Add(l + 1);
+            Sp.Add(t + 1);
+            int ribIndex = 0;
+            Ribs[ribIndex] = new int[2];
+            Ribs[ribIndex][0] = l + 1;
+            Ribs[ribIndex][1] = t + 1;
+            ribIndex++;
+            System.Collections.ArrayList tArray = new System.Collections.ArrayList();
+            for (int i = 1; i <= Nmax; i++)
+                tArray.Add(i);
+            int[] array = new int[tArray.Count];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = int.Parse(tArray[i].ToString());
+
+            while (!Contains(array, Sp))
+            {
+                min = 100;
+                l = 0; t = 0;
+                for (int i = 0; i < Nmax; i++)
+                    if (Sp.Contains(i + 1))
+                        for (int j = 0; j < Nmax; j++)
+                            if (!Sp.Contains(j + 1) && (A[i, j] < min) && (A[i, j] != 0))
+                            {
+                                min = A[i, j];
+                                l = i;
+                                t = j;
+                            }
+                A_Eiler[l, t] = A[l, t];
+                A_Eiler[t, l] = A[t, l];
+                Sp.Add(l + 1);
+                Sp.Add(t + 1);
+
+                Ribs[ribIndex] = new int[2];
+                Ribs[ribIndex][0] = l + 1;
+                Ribs[ribIndex][1] = t + 1;
+                ribIndex++;
+            }
+        }
+
+        public static bool Contains(int[] array, ISet<int> set)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!set.Contains(array[i]))
+                    return (false);
+            }
+            return (true);
+        }
+
+
+        private static void FindWay(int v)
+        {
+            for (int i = 0; i < Nmax; i++)
+                if (A_Eiler[v, i] != 0)
+                {
+                    A_Eiler[v, i] = 0;
+                    FindWay(i);
+                }
+            int[] temp = (int[])Stack.Clone();
+            Stack = new int[Stack.Length + 1];
+            for (int i = 0; i < temp.Length; i++)
+                Stack[i] = temp[i];
+            Stack[Stack.Length - 1] = v + 1;
+        }
+
+        private static void OutPut()
+        {
+            ISet<int> Way = new HashSet<int>();
+            int i, pred_v, Cost = 0;
+            Console.Write("Path: ");
+            Console.Write(" ");
+            Console.Write("{0} ", Stack[0]);
+            Way.Add(Stack[0]);
+            pred_v = Stack[0];
+            for (i = 1; i < Stack.Length; i++)
+                if (!Way.Contains(Stack[i]))
+                {
+                    Console.Write("{0} ", Stack[i]);
+                    Way.Add(Stack[i]);
+                    Cost += A[pred_v - 1, Stack[i] - 1];
+                    pred_v = Stack[i];
+                }
+            Console.WriteLine("{0} ", Stack[0]);
+            Cost += A[pred_v - 1, Stack[0] - 1];
+            Console.Write("Cost of the path: ");
+            Console.Write("{0}", Cost);
+        }
+
+        private static void PrintMatrix()
+        {
+            for (int i = 0; i < Nmax; i++) Console.Write($"\t[{i + 1}]");
+            Console.WriteLine();
+            for (int i = 0; i < Nmax; i++)
+            {
+                Console.Write($"[{i + 1}]\t");
+                for (int j = 0; j < Nmax; j++) Console.Write($"{A[i, j]}\t");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharpLab2/CSharpLab2/Program.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Register = new Register();
+            Register.Changed += ChangeListener;
+            Register.Add(new RSTrigger("Trigger1", true, false));
+            Register.Add(new JKTrigger("Trigger2", true, false));
+            Register.Add(new RSTrigger("Trigger3", false, true));
+            Register.Add(new JKTrigger("Trigger4", false, true));
+            Register.Insert(2, new RSTrigger("Trigger0", false, false));
+            Register.Add(new JKTrigger("Trigger5", false, false));
+            Register.Add(new RSTrigger("Trigger5", true, false) & new RSTrigger("Triger6", false, true));
+            Register.Delete(2);
+            Console.WriteLine(Register);
+            Register.ResetAllItems();
+            Console.WriteLine(Register);
+
+            try
+            {
+                Register.Get(48);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("IndexOutOfRangeException");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ChangeListener(ChangeType change)
+        {
+            switch (change)
+            {
+                case ChangeType.Add:
+                    Console.WriteLine("Element was added");
+                    break;
+                case ChangeType.Delete:
+                    Console.WriteLine("Element was deleted");
+                    break;
+                case ChangeType.Set:
+                    Console.WriteLine("Element was setted");
+                    break;
+                case ChangeType.Insert:
+                    Console.WriteLine("Element was inserted");
+                    break;
+                case ChangeType.Reset:
+                    Console.WriteLine("Elements were reseted");
+                    break;
+            }
+        }
+    }
+
+    public interface ITrigger
+    {
+        void Reset();
+    }
+
+    public abstract class Trigger : ITrigger
+    {
+        protected string Identificator { get; set; }
+        protected bool OutputValue { get; set; }
+
+        public abstract override string ToString();
+        public abstract override bool Equals(object obj);
+        public abstract override int GetHashCode();
+        public abstract void Reset();
+    }
+
+    public class RSTrigger : Trigger
+    {
+        private bool RInput;
+        private bool SInput;
+
+        public bool GetRInput()
+        {
+            return this.RInput;
+        }
+        public void SetRInput(bool value)
+        {
+            RInput = value;
+            SetOutputValue();
+        }
+
+        public bool GetSInput()
+        {
+            return this.SInput;
+        }
+        public void SetSInput(bool value)
+        {
+            SInput = value;
+            SetOutputValue();
+        }
+
+        private void SetOutputValue()
+        {
+            if (!RInput && SInput) OutputValue = true;
+            if (RInput && !SInput) OutputValue = false;
+            if (RInput && SInput) Console.WriteLine("Do not enter such combination");
+        }
+        public RSTrigger(string identificator, bool R, bool S)
+        {
+            this.Identificator = identificator;
+            this.RInput = R;
+            this.SInput = S;
+        }
+
+        public RSTrigger()
+        {
+            OutputValue = false;
+        }
+
+        public override void Reset()
+        {
+            SInput = false;
+            RInput = false;
+            SetOutputValue();
+        }
+
+        public RSTrigger DeepCopy()
+        {
+            RSTrigger result = new RSTrigger(this.Identificator, this.RInput, this.SInput);
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            RSTrigger o = (RSTrigger)obj;
+            if (Identificator == o.Identificator && ((RInput && o.RInput) || (SInput && o.SInput))) return true;
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return RInput.GetHashCode() + 6214 * SInput.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return " " + Identificator + " (RS-Trigger): { R = " + RInput + ", S = " + SInput + " -> " + OutputValue + " }";
+
+        }
+
+        public static RSTrigger operator &(RSTrigger x, RSTrigger y)
+        {
+            RSTrigger result = new RSTrigger
+            {
+                RInput = x.RInput & x.SInput
+            };
+            return result;
+        }
+
+        public static RSTrigger operator |(RSTrigger x, RSTrigger y)
+        {
+            RSTrigger result = new RSTrigger
+            {
+                RInput = x.RInput | x.SInput
+            };
+            return result;
+        }
+
+        public static bool operator ==(RSTrigger x, RSTrigger y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(RSTrigger x, RSTrigger y)
+        {
+            return !Equals(x, y);
+        }
+
+    }
+
+    public class JKTrigger : Trigger
+    {
+        private bool JInput;
+        private bool KInput;
+
+        public bool GetRInput()
+        {
+            return this.JInput;
+        }
+        public void SetRInput(bool value)
+        {
+            JInput = value;
+            SetOutputValue();
+        }
+
+        public bool GetSInput()
+        {
+            return this.KInput;
+        }
+        public void SetSInput(bool value)
+        {
+            KInput = value;
+            SetOutputValue();
+        }
+
+        private void SetOutputValue()
+        {
+            if (!JInput && KInput) OutputValue = true;
+            if (JInput && !KInput) OutputValue = false;
+            if (JInput && KInput) OutputValue = !OutputValue;
+        }
+        public JKTrigger(string identificator, bool J, bool K)
+        {
+            this.Identificator = identificator;
+            this.JInput = J;
+            this.KInput = K;
+        }
+
+        public JKTrigger()
+        {
+            OutputValue = false;
+        }
+
+
+        public override void Reset()
+        {
+            KInput = false;
+            JInput = false;
+            SetOutputValue();
+        }
+
+        public JKTrigger DeepCopy()
+        {
+            JKTrigger result = new JKTrigger(this.Identificator, this.JInput, this.KInput);
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            JKTrigger o = (JKTrigger)obj;
+            if (Identificator == o.Identificator && ((JInput && o.JInput) || (KInput && o.KInput))) return true;
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return JInput.GetHashCode() + 6214 * KInput.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return " " + Identificator + " (JK-Trigger): { J = " + JInput + ", K = " + KInput + " -> " + OutputValue + " }";
+
+        }
+
+        public static JKTrigger operator &(JKTrigger x, JKTrigger y)
+        {
+            JKTrigger result = new JKTrigger
+            {
+                JInput = x.JInput & x.KInput
+            };
+            return result;
+        }
+
+        public static JKTrigger operator |(JKTrigger x, JKTrigger y)
+        {
+            JKTrigger result = new JKTrigger
+            {
+                JInput = x.JInput | x.KInput
+            };
+            return result;
+        }
+
+        public static bool operator ==(JKTrigger x, JKTrigger y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(JKTrigger x, JKTrigger y)
+        {
+            return !Equals(x, y);
+        }
+
+
+    }
+
+    public enum ChangeType { Add, Insert, Delete, Set, Reset }
+    public delegate void ChangeHandler(ChangeType change);
+
+    public class Register
+    {
+        public List<Trigger> Elements
+        {
+            get;
+        }
+
+        public event ChangeHandler Changed;
+
+        public Register()
+        {
+            Elements = new List<Trigger>();
+        }
+
+        public void Add(Trigger v)
+        {
+            Elements.Add(v);
+            Changed?.Invoke(ChangeType.Add);
+        }
+
+        public Trigger Get(int index)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            return Elements[index];
+        }
+
+        public void Set(int index, Trigger v)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            Elements[index] = v;
+            Changed?.Invoke(ChangeType.Set);
+        }
+
+        public void Insert(int index, Trigger v)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            Elements.Insert(index, v);
+            Changed?.Invoke(ChangeType.Insert);
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= Elements.Count) throw new IndexOutOfRangeException();
+            Elements.RemoveAt(index);
+            Changed?.Invoke(ChangeType.Delete);
+        }
+
+        public void ResetAllItems()
+        {
+            Trigger obj;
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                obj = Get(i);
+                obj.Reset();
+            }
+            Changed?.Invoke(ChangeType.Reset);
+        }
+
+        public override string ToString()
+        {
+            return "[" + string.Join("\n", Elements) + "]";
+        }
+    }
+
+    public class MyExeption : Exception
+    {
+        MyExeption(String str) : base("From MyExeption: " + str) { }
+    }
+}
